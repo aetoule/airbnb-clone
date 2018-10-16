c82595355bb475b5d81916544c63b0f8586e95a8
Aisha Toulegenova <aetoule@gmail.com>
2018-10-16 15:03:00 -0700

    oneHome component rendering image list

--- server/controller.js
+++ server/controller.js
@@ -40,65 +40,62 @@ module.exports = {
         dbInstance = req.app.get('db');
         let { city } = req.body;
         dbInstance.get_homes_in_one_city('Phoenix')
-            .then(homes => {
-                let reduced = homes.reduce((prev, curr) => {
-                    let { home_id,
-                        home_name,
-                        price, max_guests,
-                        describe_main,
-                        describe_space,
-                        describe_guest_access,
-                        describe_interaction_with_guests,
-                        describe_other_things_to_note,
-                        address,
-                        img_url,
-                        city, } = curr;
+        .then(homes => {
+            let reduced = homes.reduce((prev, curr) => {
+                let { home_id,
+                    home_name,
+                    price, max_guests,
+                    describe_main,
+                    describe_space,
+                    describe_guest_access,
+                    describe_interaction_with_guests,
+                    describe_other_things_to_note,
+                    address,
+                    img_url,
+                    city, } = curr;
 
-                    prev[home_id] = prev[home_id] || {
-                        home_id,
-                        home_name,
-                        price,
-                        max_guests,
-                        describe_main,
-                        describe_space,
-                        describe_guest_access,
-                        describe_interaction_with_guests,
-                        describe_other_things_to_note,
-                        address,
-                        city,
-                        imgs: []
-                    };
-                    prev[home_id].imgs.push({
-                        img_url,
-                        main: Boolean(curr.main_image)
-                    });
-                    return prev;
-                }, {});
-                let homeArray = Object.keys(reduced).map(key => reduced[key]);
+                prev[home_id] = prev[home_id] || {
+                    home_id,
+                    home_name,
+                    price,
+                    max_guests,
+                    describe_main,
+                    describe_space,
+                    describe_guest_access,
+                    describe_interaction_with_guests,
+                    describe_other_things_to_note,
+                    address,
+                    city,
+                    imgs: []
+                };
+                prev[home_id].imgs.push({
+                    img_url,
+                    main: Boolean(curr.main_image)
+                });
+                return prev;
+            }, {});
+            let homeArray = Object.keys(reduced).map(key => reduced[key]);
 
-                res.status(200).send(homeArray);
-            })
-            .catch(err => {
-                res.status(500).send(err)
-            })
-
-
+            res.status(200).send(homeArray);
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
     },
 
     getOneHome: (req, res) => {
         dbInstance = req.app.get('db');
-        let { id } = req.params;
+        let {id} = req.params;
         console.log(id)
         dbInstance.get_one_home(id)
-            .then(home => {
-                console.log(home)
-                res.status(200).send(home)
-            })
+        .then(home => {
+            console.log(home)
+            res.status(200).send(home)})
 
-            .catch(err => {
-                res.status(500).send(err)
-            })
-
+        .catch(err => {
+            res.status(500).send(err)
+        })
+            
     }
     // createHome: (req, res) => {
     //     const dbInstance = req.app.get('db');
