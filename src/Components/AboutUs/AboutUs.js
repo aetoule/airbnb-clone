6f2310812c9cf09c5a18779132aab62d1fd7524e
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-11-01 10:26:42 -0700

    add portfolio to about page

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -4,10 +4,10 @@ import './AboutUs.css';
 class AboutUs extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className="about-us-container">
                 <div className="about-us-text-container">
                     <h1 className="get-to-know-us-text">Get To Know Us</h1>
@@ -18,17 +18,20 @@ class AboutUs extends Component {
                     <div className="bios">
                         <div className="kaylin-bio">
                             <h2>Kaylin</h2>
-                            <p></p>
+                            <p>Hi! My name is Kaylin Anderson and I am a Web Developer with a passion for learning new, exciting technologies. I have recently graduated from a fully immersive, Web Development boot-camp and am excited to hit the ground running!</p>
+                            <button className="portfolio-button"><a href="https://kaylinann.squarespace.com/" target="_blank">Check out my Portfolio!</a></button>
                         </div>
                         <div className="aisha-bio">
                             <h2>Aisha</h2>
                             <p>Aisha is a UX Designer turned web developer from Seattle. She loves to drink coffee and explore her newly discovered home city of Phoenix. She is extremely excited to jump into the industry as a front-end web developer</p>
+                            <button className="portfolio-button" ><a href="https://aishatoulegenova.com/" target="_blank">Check out my Portfolio!</a>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default AboutUs;
