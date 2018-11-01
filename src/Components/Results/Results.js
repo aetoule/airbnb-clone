2cd6c9fa198b0c9dc6961bba69e538720ae79cd2
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-11-01 10:09:28 -0700

    added styling and date search to results comp

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -4,11 +4,12 @@ import { connect } from 'react-redux';
 import { getAllHomes, getCityHomes, getCity } from '../../redux/reducer';
 import { Link } from "react-router-dom";
 import Search from '../Search/Search';
+import './Results.css';
 
 class Results extends Component {
 
   state = {
-
+    searchToggle: false
   }
 
   componentDidMount() {
@@ -22,7 +23,16 @@ class Results extends Component {
     if (this.props.city !== prevProps.city) {
       this.getResults(this.props.city)
     }
+    if (this.props.endDate !== prevProps.endDate & this.props.endDate != false) {
+      this.setState({
+        searchToggle: false
+      })
+    }
   }
+
+  // componentDidUpdate(prevProps) {
+
+  // }
 
   getHomes() {
     axios.get('/api/homes').then(res => {
@@ -52,7 +62,7 @@ class Results extends Component {
         console.log(e.imgs.length);
         if (e.imgs.length !== 0) {
           return (
-            <div key={e.homeid}>
+            <div className='mappedList' key={e.homeid}>
               <div>
                 <Link to={`/results/${e.homeid}`}>
                   <img className='results-images' src={e.imgs[0].img_url} alt="homes main image" />
@@ -70,20 +80,28 @@ class Results extends Component {
                 <h5>
                   ${e.price} per night
               </h5>
+                <div className="five-stars">
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <h6 className="ratings-number-oneHome">751</h6>
+                </div>
               </div>
             </div>
           )
         } else {
           return (
-            <div key={e.homeid}>
+            <div className='mappedList' key={e.homeid}>
               <div>
                 <Link to={`/results/${e.homeid}`}>
                   <img className='results-images' src='https://files.slack.com/files-pri/T039C2PUY-FDQFY86A3/defaultimage.png' alt="homes main image" />
                 </Link>
 
 
-              </div>
-              <div>
+
+
                 <h5>
                   {e.max_guests} Guests
               </h5>
@@ -93,6 +111,14 @@ class Results extends Component {
                 <h5>
                   ${e.price} per night
               </h5>
+                <div className="five-stars">
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <div className='one-star'></div>
+                  <h6 className="ratings-number-oneHome">751</h6>
+                </div>
               </div>
             </div>
           )
@@ -105,15 +131,15 @@ class Results extends Component {
         console.log(e);
 
         return (
-          <div key={e.homeid}>
+          <div className='mappedList' key={e.homeid}>
             <div>
               <Link to={`/results/${e.homeid}`}>
                 <img className='results-images' src={e.imgs[0].img_url} alt="homes main image" />
               </Link>
 
 
-            </div>
-            <div>
+
+
               <h5>
                 {e.max_guests} Guests
             </h5>
@@ -123,20 +149,25 @@ class Results extends Component {
               <h5>
                 ${e.price} per night
             </h5>
+              <div className="five-stars">
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <h6 className="ratings-number-oneHome">751</h6>
+              </div>
             </div>
           </div>
         )
       } else {
         return (
-          <div key={e.homeid}>
+          <div className='mappedList' key={e.homeid}>
             <div>
               <Link to={`/results/${e.homeid}`}>
                 <img className='results-images' src='https://files.slack.com/files-pri/T039C2PUY-FDQFY86A3/defaultimage.png' alt="homes main image" />
               </Link>
 
-
-            </div>
-            <div>
               <h5>
                 {e.max_guests} Guests
             </h5>
@@ -146,6 +177,14 @@ class Results extends Component {
               <h5>
                 ${e.price} per night
             </h5>
+              <div className="five-stars">
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <div className='one-star'></div>
+                <h6 className="ratings-number-oneHome">751</h6>
+              </div>
             </div>
           </div>
         )
@@ -154,31 +193,56 @@ class Results extends Component {
 
     })
 
+    const dates = this.props.endDate ?
+      <div>{this.props.startDate._d.getMonth() + 1}/{this.props.startDate._d.getDate()}/{this.props.startDate._d.getFullYear()} - {this.props.endDate._d.getMonth() + 1}/{this.props.endDate._d.getDate()}/{this.props.endDate._d.getFullYear()}</div> :
+      "Dates"
+
+    const searchBar = this.state.searchToggle ?
+      <div>
+        <button className='results-search-btn' onClick={() => this.setState({ searchToggle: false })}>X</button>
+        <Search />
+      </div> :
+      <div>
+        <button className='results-search-btn' onClick={() => this.setState({ searchToggle: true })}>{dates}</button>
+      </div>
+
+    const listingsTitle = this.props.city ?
+      <div>
+        <h1>Homes in {this.props.city}</h1>
+      </div> :
+      <div>
+        <h1>Top-rated homes</h1>
+      </div>
 
     return (
       <div className="Results">
 
         <div className='users-dates'>
-          <div>
-            <Search> </Search>
+          <div className='results-searchbar'>
+            {searchBar}
           </div>
         </div>
         <div>
+          {listingsTitle}
           200+ Homes...
         </div>
-        {home}
-        {mappedHomes}
+        <div className='results-image-container'>
+          {home}
+          {mappedHomes}
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  const { homes, cityHomes, city } = state;
+  const { homes, cityHomes, city, startDate, endDate } = state;
   return {
     homes,
     cityHomes,
-    city
+    city,
+    startDate,
+    endDate,
   }
 }
 
